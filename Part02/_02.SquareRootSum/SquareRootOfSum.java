a97d88e83528c46147cea502b0d471f738707c01
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2020-08-23 10:52:25 -0500

    Create SquareRootOfSum.java

---
+++ Part02/_02.SquareRootSum/SquareRootOfSum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        int total = first + second;
+        double sqRoot = Math.sqrt(total);
+        System.out.println(sqRoot);
+    }
+}
