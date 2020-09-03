0cbd1850bba6d140943b3e84e3fedef8bc604c21
Michael Shoemaker <mikeshoemaker2000@yahoo.com>
2020-09-02 19:39:20 -0500

    Create LineByLine.java

---
+++ Part3/_26.LineByLine/LineByLine.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LineByLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            String [] split = input.split(" ");
+        
+            for(String part: split){
+                System.out.println(part);
+            }
+        }
+    
+    }
+}
